77512cce46b9695b5078ef526403472e649d611d
William Clemmons Jr <william.clemmons.jr@gmail.com>
2025-01-04 22:52:31 -0600

    changes speed down to 10 because of motor issues

--- LightySwitch/LightySwitch.ino
+++ LightySwitch/LightySwitch.ino
@@ -12,11 +12,11 @@
 #include <Stepper.h>
 
 const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
-const int rolePerMinute = 11;         // Adjustable range of 28BYJ-48 stepper is 0~17 rpm
+const int rolePerMinute = 10;         // Adjustable range of 28BYJ-48 stepper is 0~17 rpm
 
-// initialize the stepper library on pins 8 through 11:
-// Stepper myStepper(stepsPerRevolution, 9, 10, 13, 14);
+// initialize the stepper library on pins
 Stepper myStepper(stepsPerRevolution, 5, 6, 9, 10);
+
 void setup() {
   myStepper.setSpeed(rolePerMinute);
   // initialize the serial port:
