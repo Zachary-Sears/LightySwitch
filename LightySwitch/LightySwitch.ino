01247033a65950384c737fc64e64040a3f975744
zachary-sears <zears10@gmail.com>
2025-01-04 18:58:35 -0600

    Initial motion detection enabled

--- LightySwitch/LightySwitch.ino
+++ LightySwitch/LightySwitch.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 13;  // LED on Pin 13 of Arduino
+int pirPin = DD3; // Input for HC-S501
 
+int pirValue; // Place to store read PIR Value
+
+
+void setup() {
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(pirPin, INPUT);
+ 
+  digitalWrite(ledPin, LOW);
+  
+  
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  pirValue = digitalRead(pirPin);
+  digitalWrite(ledPin, pirValue);
 
 }
