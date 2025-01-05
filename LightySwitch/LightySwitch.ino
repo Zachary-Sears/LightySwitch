5d71f489dc5e7acfb8439759983479e9ab41b982
zachary-sears <zears10@gmail.com>
2025-01-04 19:05:14 -0600

    Extended comment on line 2 to indicate which pin is being used for sensor input

--- LightySwitch/LightySwitch.ino
+++ LightySwitch/LightySwitch.ino
@@ -1,5 +1,5 @@
 int ledPin = 13;  // LED on Pin 13 of Arduino
-int pirPin = DD3; // Input for HC-S501
+int pirPin = DD3; // Input for HC-S501 corresponds to pin D3 on Nano
 
 int pirValue; // Place to store read PIR Value
 
